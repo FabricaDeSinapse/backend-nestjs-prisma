6e5a6bff281365012a88c0af0f96d70f2ead4d74
Paulo Salvatore <salvatoregames@gmail.com>
2021-03-19 20:10:09 -0300

    feat(pokemon): create `Entity` and `CreateDto`

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,11 @@
-export class CreatePokemonDto {}
+import { Pokemon } from '../entities/pokemon.entity';
+import { IsInt, IsOptional, IsString } from 'class-validator';
+
+export class CreatePokemonDto extends Pokemon {
+  @IsString()
+  name: string;
+
+  @IsInt()
+  @IsOptional()
+  height?: number | null;
+}
--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,8 @@
-export class Pokemon {}
+import { Prisma } from '@prisma/client';
+
+export class Pokemon implements Prisma.PokemonUncheckedCreateInput {
+  id?: number;
+  name: string;
+  height?: number;
+  images?: Prisma.ImageUncheckedCreateNestedManyWithoutPokemonInput;
+}
