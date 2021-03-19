b5d2e83cde453fe28e7d36af6477b9ce76c04f77
Paulo Salvatore <salvatoregames@gmail.com>
2021-03-19 20:10:31 -0300

    feat(pokemon): add service methods to `create` and `readAll`

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -7,12 +7,12 @@ import { PrismaService } from '../prisma/prisma.service';
 export class PokemonService {
   constructor(private readonly prisma: PrismaService) {}
 
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+  create(data: CreatePokemonDto) {
+    return this.prisma.pokemon.create({ data });
   }
 
   findAll() {
-    return `This action returns all pokemon`;
+    return this.prisma.pokemon.findMany();
   }
 
   findOne(id: number) {
