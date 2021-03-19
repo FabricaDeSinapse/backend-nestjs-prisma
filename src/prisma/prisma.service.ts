93df94635636b4815489b6fb1b80c54c45bb2a5f
Paulo Salvatore <salvatoregames@gmail.com>
2021-03-19 19:54:41 -0300

    fix(prisma): fix `PrismaClient` import

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client/scripts/default-index';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
