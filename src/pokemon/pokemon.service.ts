3fa3cd39ef9afab4854ad3c994d302a3cd7a8329
Paulo Salvatore <salvatoregames@gmail.com>
2021-03-19 20:12:30 -0300

    feat(pokemon): add service methods to `update`, `delete` and `findOne`

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -16,14 +16,21 @@ export class PokemonService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+    return this.prisma.pokemon.findUnique({
+      where: { id },
+    });
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  update(id: number, data: UpdatePokemonDto) {
+    return this.prisma.pokemon.update({
+      where: { id },
+      data,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+    return this.prisma.pokemon.delete({
+      where: { id },
+    });
   }
 }
